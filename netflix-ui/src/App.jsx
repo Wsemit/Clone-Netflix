84c5ee0c993f6cb42e167ce4f526721c44897b2c
Wsemit <dedyaev.ua@gmail.com>
2025-08-01 14:22:54 +0300

    feat: setup routing for login, signup, player, movies, tv shows, my list

--- netflix-ui/src/App.jsx
+++ netflix-ui/src/App.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import MoviePage from "./pages/Movies";
 import Netflix from "./pages/Netflix";
+import Player from "./pages/Player";
+import Signup from "./pages/Signup";
+import TVShows from "./pages/TVshows";
+import UserListedMovies from "./pages/UserListedMovies";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/netflix" element={<Netflix />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/player" element={<Player />} />
+        <Route exact path="/tv" element={<TVShows />} />
+        <Route exact path="/movies" element={<MoviePage />} />
+        <Route exact path="/new" element={<Player />} />
+        <Route exact path="/mylist" element={<UserListedMovies />} />
+        <Route exact path="/" element={<Netflix />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
