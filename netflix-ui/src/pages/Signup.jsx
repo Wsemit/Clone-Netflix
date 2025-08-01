55a6c8ecfebd5d41f99fef81837b61dfb5cc0c6c
Wsemit <dedyaev.ua@gmail.com>
2025-08-01 12:15:11 +0300

    Setup Signup, BackgroundImages, Header

--- netflix-ui/src/pages/Signup.jsx
+++ netflix-ui/src/pages/Signup.jsx
@@ -1,5 +1,25 @@
 import React from "react";
-
+import styled from "styled-components";
+import BackgroundImages from "../components/BackgroundImages";
+import Header from "../components/Header";
 export default function Signup() {
-    return <div>Signup</div>;
+    return <SignupContainer>
+        <BackgroundImages />
+        <Header />
+        <div className="body flex column a-center j-center">
+            <div className="text flex colum">
+                <h1>Unlimited movies, TV shows, and more.</h1>
+                <h4>Watch anywhere. Cancel anytime.</h4>
+                <h6>Ready to watch? Enter your email to create or restart your membership.</h6>
+            </div>
+            <div className="form">
+                <input type="email" placeholder="Email address" name="email" />
+                <input type="password" placeholder="Password" name="password" />
+                <button>Get Started</button>
+            </div>
+            <button>Log In</button>
+        </div>
+    </SignupContainer>;
 }
+
+const SignupContainer = styled.div``;
--- netflix-ui/src/utils/firebase-config.js
+++ netflix-ui/src/utils/firebase-config.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCpDDv2VJgomNi1BVQ6n6vO4LAwKfgAJ_g",
   authDomain: "react-netflix-9479d.firebaseapp.com",
@@ -16,6 +12,6 @@ const firebaseConfig = {
   measurementId: "G-CX3GWNNTRW"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const FirebaseAuth = getAuth(app);
